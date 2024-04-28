9f5ca807f607e23bf72d6959b13c82c2d2585eae
Shi3ofreniR <t40411304@yandex.ru>
2024-04-28 22:24:41 +0700

    Initial commit

---
+++ src/main/java/mod/nero/toastsounds/mixin/AdvMixin.java
@@ -0,0 +1,44 @@
+package mod.nero.toastsounds.mixin;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.advancements.Advancement;
+import net.minecraft.advancements.DisplayInfo;
+import net.minecraft.advancements.FrameType;
+import net.minecraft.client.gui.components.toasts.AdvancementToast;
+import net.minecraft.client.gui.components.toasts.Toast;
+import net.minecraft.client.gui.components.toasts.ToastComponent;
+import net.minecraft.client.resources.sounds.SimpleSoundInstance;
+import net.minecraft.sounds.SoundEvents;
+import org.spongepowered.asm.mixin.Final;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import mod.nero.toastsounds.ModSounds;
+
+@Mixin(AdvancementToast.class)
+public abstract class AdvMixin {
+    @Final
+    @Shadow
+    private Advancement advancement;
+    @Shadow
+    private boolean playedSound;
+
+    @Inject(method = "render", at = @At(value = "HEAD"))
+    protected void advtoast(PoseStack p_94800_, ToastComponent p_94801_, long p_94802_, CallbackInfoReturnable<Toast.Visibility> cir)
+    {
+        DisplayInfo dspnfo = this.advancement.getDisplay();
+        if (!this.playedSound) {
+            assert dspnfo != null;
+            if(dspnfo.getFrame() == FrameType.CHALLENGE)
+                p_94801_.getMinecraft().getSoundManager().play(SimpleSoundInstance.forUI(SoundEvents.UI_TOAST_CHALLENGE_COMPLETE, 1.0F, 1.0F));
+            else if(dspnfo.getFrame() == FrameType.GOAL)
+                p_94801_.getMinecraft().getSoundManager().play(SimpleSoundInstance.forUI(ModSounds.GOAL_NOTIFY.get(), 1.0F, 1.0F));
+            else
+                p_94801_.getMinecraft().getSoundManager().play(SimpleSoundInstance.forUI(ModSounds.ADV_NOTIFY.get() , 1.0F, 1.0F));
+            this.playedSound = true;
+
+        }
+    }
+}
---
+++ src/main/java/mod/nero/toastsounds/mixin/RecMixin.java
@@ -0,0 +1,28 @@
+package mod.nero.toastsounds.mixin;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.minecraft.client.gui.components.toasts.RecipeToast;
+import net.minecraft.client.gui.components.toasts.Toast;
+import net.minecraft.client.gui.components.toasts.ToastComponent;
+import net.minecraft.client.resources.sounds.SimpleSoundInstance;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+import mod.nero.toastsounds.ModSounds;
+
+@Mixin(RecipeToast.class)
+public abstract class RecMixin {
+    @Shadow
+    private boolean changed;
+    @Inject(method = "render", at = @At(value = "HEAD"))
+    protected void crafttoast(PoseStack p_94814_, ToastComponent p_94815_, long p_94816_, CallbackInfoReturnable<Toast.Visibility> cir) {
+        if (this.changed) {
+            p_94815_.getMinecraft().getSoundManager().play(SimpleSoundInstance.forUI(ModSounds.RECIPE_NOTIFY.get(), 1.0F, 1.0F));
+        }
+
+    }
+
+
+}
